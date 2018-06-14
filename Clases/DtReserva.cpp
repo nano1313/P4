9d4356694e68a46c3db296351705a8dfc9057dde
Franco <nano.13.13@hotmail.com>
2018-06-14 13:42:59 -0300

    dtreserva y algo mas

---
+++ Clases/DtReserva.cpp
@@ -0,0 +1,47 @@
+#include "../Definiciones/DtReserva.hpp"
+DtReserva::DtReserva (string titulo, DtFecha fecha, DtHora hora, float precio, int asientos, int cine) {
+    this->tituloPelicula = titulo;
+    this->fecha = fecha;
+    this->hora = hora;
+    this->precio = precio;
+    this->asientos = asientos;
+    this->numeroCine = cine;
+}
+/* Getters */
+string DtReserva::getTituloPelicula() {
+    return this->tituloPelicula;
+}
+DtFecha DtReserva::getFecha() {
+    return this->fecha;
+}
+DtHora DtReserva::getHora() {
+    return this->hora;
+}
+float DtReserva::getPrecio() {
+    return this->precio;
+}
+int DtReserva::getAsiento() {
+    return this->asientos;
+}
+int DtReserva::getNumeroCine() {
+    return this->numeroCine;
+}
+/* Setters */
+void DtReserva::setTituloPelicula(string titulo) {
+     this->tituloPelicula = titulo;
+}
+void DtReserva::setFecha(DtFecha fecha) {
+     this->fecha = fecha;
+}
+void DtReserva::setHora(DtHora hora) {
+     this->hora = hora;
+}
+void DtReserva::setPrecio(float precio) {
+     this->precio = precio;
+}
+void DtReserva::setAsiento(int asientos) {
+     this->asientos = asientos;
+}
+void DtReserva::setNumeroCine(int numero) {
+     this->numeroCine = numero;
+}
